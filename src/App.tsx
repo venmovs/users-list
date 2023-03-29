55b3a7682571fd024c39fe3e67ad784d4c483877
Vigen Movsisyan <vigen94@icloud.com>
2023-03-29 15:26:40 +0400

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import UserList from './components/UserList';
+import UserDetails from './components/UserDetails';
 
 function App() {
   return (
-    <div>
-      User List
-    </div>
+    <Routes>
+      <Route path="/" element={<UserList />} />
+      <Route path="/:id" element={<UserDetails />} />
+    </Routes>
   );
 }
 
