97579c70ca1770c21bc5fe8639bb91d06cabdc49
Vigen Movsisyan <vigen94@icloud.com>
2023-03-30 00:30:06 +0400

    loading component

--- src/components/UI/Loading/index.tsx
+++ src/components/UI/Loading/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import style from './Loading.module.scss';
 
 function Loading() {
-  return <div>loading...</div>;
+  return (
+    <div className={style.loading}>
+      <Typography variant="h6">
+        Loading will take a few seconds..
+      </Typography>
+      <CircularProgress />
+    </div>
+  );
 }
 
 export default Loading;
