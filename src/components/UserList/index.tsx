ab7a0072371ccc4e414d82b4abe10c91b8d7d6f4
Vigen Movsisyan <vigen94@icloud.com>
2023-03-30 20:15:57 +0400

    search

--- src/components/UserList/index.tsx
+++ src/components/UserList/index.tsx
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
+import {
+  Box, FormControl, InputLabel, OutlinedInput,
+} from '@mui/material';
 import { useGetUsersQuery } from '../../services/users-api';
 import Loading from '../UI/Loading';
 import Error from '../UI/Error';
 import ListItem from '../ListItem';
 import style from './UserList.module.scss';
+import { User } from '../../services/types';
+import { StringKeys } from './types';
 
 function UserList() {
-  const { isLoading, isError, data: users } = useGetUsersQuery();
+  const { isLoading, isError, data } = useGetUsersQuery();
+  const [searchValue, setSearchValue] = useState<string>('');
+  const [users, setUsers] = useState<User[] | undefined>([]);
+
+  useEffect(() => {
+    if (data) {
+      setUsers(data);
+    }
+  }, [data]);
+
+  const filterUsers = (targetUser: string, targetParameter: StringKeys<User>) => {
+    if (!data) {
+      return [];
+    }
+    const usersCopy = [...data];
+    const regex = new RegExp(targetUser, 'gi');
+    return usersCopy.filter((user) => user[targetParameter].match(regex));
+  };
+
+  const changeSearchValue = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    setSearchValue(inputValue);
+    const foundByNames = filterUsers(inputValue, 'name');
+    const foundByEmail = filterUsers(inputValue, 'email');
+    const uniqElements = new Set([...foundByNames, ...foundByEmail]);
+    setUsers(Array.from(uniqElements));
+    if (!inputValue.length) {
+      setUsers(data);
+    }
+  };
 
   const reload = () => {
     window.location.reload();
@@ -24,7 +58,22 @@ function UserList() {
 
   return (
     <main className={style.userList}>
-      {renderUsers}
+      <Box p={1}>
+        <FormControl fullWidth>
+          <InputLabel htmlFor="search">
+            Find user
+          </InputLabel>
+          <OutlinedInput
+            value={searchValue}
+            onChange={changeSearchValue}
+            id="search"
+            label="Find user"
+          />
+        </FormControl>
+      </Box>
+      <section className={style.userList__listWrapper}>
+        {renderUsers}
+      </section>
     </main>
   );
 }
